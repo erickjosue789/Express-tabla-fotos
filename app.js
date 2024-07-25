1cb8ea76e3e8b86e72fd5444403bb61855692d2c
Erick Valdiviezo <erickvaldiviezo@Laptop-de-Erick.local>
2024-07-25 15:35:31 -0500

    Uso de apis

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fotosRouter = require('./routes/fotos');
+var fotosRo = require('./rest/fotos');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fotos', fotosRouter);
+app.use('/rest/fotos', fotosRo);
 
 
 // catch 404 and forward to error handler
